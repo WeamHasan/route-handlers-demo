b619d3ce918865ab5a17cbf0e772194696900373
Weam Hassan <weamhf25@gmail.com>
2025-11-19 09:24:26 +0200

    Test commit

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,6 +1,6 @@
 
 export default function ProfilePage() {
     return (
-        <h1>Profile Page</h1>
+        <h1 className="text-center text-3xl sm:text-4xl font-extrabold text-gray-900">Profile Page</h1>
     )
 }
